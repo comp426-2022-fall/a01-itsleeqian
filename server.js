3d4dce986c8a40e58552c3647c32ab4dd90045f2
Lee Qi An <itsleeqian@gmail.com>
2022-09-07 18:31:45 -0400

    bug fix attempt 2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist')
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port||3000
+const port = argv.port||3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -31,7 +31,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 	const server = http.createServer((req, res) => {
 		res.statusCode = 200
 		res.setHeader(('Content-Type', 'text/html'))
-		res.end(fileData)
+		res.end(data)
 	})
 
 	// Start the `server` const listening on the port defined by argument in your `port` const. 
